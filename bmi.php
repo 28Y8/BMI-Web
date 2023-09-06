d1b5b11bd91532695d62660d71a67e833ca89b68
28Y8 <46650900+28Y8@users.noreply.github.com>
2023-09-06 15:51:18 +0000

    New update, i hate this

--- bmi.php
+++ bmi.php
@@ -1,55 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["calculate"])) {
+    // Get user input
+    $name = $_POST["name"];
+    $weight = $_POST["weight"];
+    $height = $_POST["height"];
+    $gender = $_POST["gender"];
 
-<head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="style.css">
-      <title>BMI Result</title>
-</head>
+    // Check if inputs are valid numbers
+    if (!is_numeric($weight) || !is_numeric($height)) {
+        echo "<p>Please enter valid numeric values for weight and height.</p>";
+    } else {
+        // Calculate BMI
+        $heightInMeters = $height / 100;
+        $bmi = $weight / ($heightInMeters * $heightInMeters);
 
-<body>
-      <div class="top">
-            <h1>BMI Result</h1>
-      </div>
-      
-      <div class="result">
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["calculate"])) {
-                  // Get user input
-                  $name = $_POST["name"];
-                  $weight = $_POST["weight"];
-                  $height = $_POST["height"];
-                  $gender = $_POST["gender"];
+        // Determine BMI category
+        if ($bmi < 18.5) {
+            $category = "Underweight";
+        } elseif ($bmi >= 18.5 && $bmi < 24.9) {
+            $category = "Normal Weight";
+        } elseif ($bmi >= 25 && $bmi < 29.9) {
+            $category = "Overweight";
+        } else {
+            $category = "Obese";
+        }
 
-                  // Check if inputs are valid numbers
-                  if (!is_numeric($weight) || !is_numeric($height)) {
-                        echo "<p>Please enter valid numeric values for weight and height.</p>";
-                  } else {
-                        // Calculate BMI
-                        $heightInMeters = $height / 100;
-                        $bmi = $weight / ($heightInMeters * $heightInMeters);
-
-                        // Determine BMI category
-                        if ($bmi < 18.5) {
-                              $category = "Underweight";
-                        } elseif ($bmi >= 18.5 && $bmi < 24.9) {
-                              $category = "Normal Weight";
-                        } elseif ($bmi >= 25 && $bmi < 29.9) {
-                              $category = "Overweight";
-                        } else {
-                              $category = "Obese";
-                        }
-
-                        // Display the result
-                        echo "<p>Name: " . $name . "</p>";
-                        echo "<p>Gender: " . $gender . "</p>";
-                        echo "<p>Your BMI is: " . number_format($bmi, 2) . "</p>";
-                        echo "<p>This is considered: " . $category . "</p>";
-                  }
-            }
-            ?>
-      </div>
-</body>
-
-</html>
+        // Display the result
+        echo "<p>Name: " . $name . "</p>";
+        echo "<p>Gender: " . $gender . "</p>";
+        echo "<p>Your BMI is: " . number_format($bmi, 2) . "</p>";
+        echo "<p>This is considered: " . $category . "</p>";
+    }
+}
